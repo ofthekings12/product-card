6e6a53377ea43a59f293ba2f75e1af372e5b0725
Mikko De Los Reyes <mikkode212@gmail.com>
2023-01-18 14:06:56 -0500

    imported fonts and applied to product details

--- frontend_product-card/src/App.js
+++ frontend_product-card/src/App.js
@@ -7,14 +7,14 @@ function App() {
       <div className="container">
         <img className="product-img" src={image1} alt="perfume"></img>
         <div className="product-details">
-          <h1 className="header">P E R F U M E</h1>
-          <h2 className="product-name">Gabrielle Essence Eau De Parfum</h2>
-          <h3 className="product-description">A floral, solar and voluptuous interpretation composed by Olivier Polge, Perfumer-Creator for the House of CHANEL.</h3>
+          <div className="header">P E R F U M E</div>
+          <div className="product-name">Gabrielle Essence Eau De Parfum</div>
+          <div className="product-description">A floral, solar and voluptuous interpretation composed by Olivier Polge, Perfumer-Creator for the House of CHANEL.</div>
         <div className="prices">
           <div className="current-price">$149.99</div>
           <div className="previous-price">$169.99</div>
         </div>
-        <button>Add to cart</button>
+        <button className="button">Add to Cart</button>
           </div>
       </div>
     </div>
