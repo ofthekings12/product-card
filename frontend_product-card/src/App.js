7fa714595492186b289e2b9bcf72f6b8fcc09599
Mikko De Los Reyes <mikkode212@gmail.com>
2023-01-18 13:09:59 -0500

    image added, resized, and overflow hidden

--- frontend_product-card/src/App.js
+++ frontend_product-card/src/App.js
@@ -1,14 +1,11 @@
 import "./App.scss";
-import image1 from "./images/image1.jpg"
+import image1 from "./images/image1.jpg";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
-        <div className="product-img">
-          <img src={image1} alt="perfume"></img>
-        </div>
-        
+        <img className="product-img" src={image1} alt="perfume"></img>
         <div className="product-details">product details</div>
       </div>
     </div>
