465da98004628e7e6e2d0b250b004245f5d72e30
Mikko De Los Reyes <mikkode212@gmail.com>
2023-01-18 12:54:29 -0500

    added product image, needs resizing

--- frontend_product-card/src/App.js
+++ frontend_product-card/src/App.js
@@ -1,10 +1,15 @@
-import './App.css';
+import "./App.scss";
+import image1 from "./images/image1.jpg"
 
 function App() {
   return (
     <div className="App">
       <div className="container">
-        hello
+        <div className="product-img">
+          <img src={image1} alt="perfume"></img>
+        </div>
+        
+        <div className="product-details">product details</div>
       </div>
     </div>
   );
