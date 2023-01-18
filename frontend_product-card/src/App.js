73076acbe52169cc77be4040f023617f9f29b15c
Mikko De Los Reyes <mikkode212@gmail.com>
2023-01-18 12:19:08 -0500

    created background with color palette, created div and centered

--- frontend_product-card/src/App.js
+++ frontend_product-card/src/App.js
@@ -1,9 +1,11 @@
-
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-     <h1>Hello World!</h1>
+      <div className="container">
+        hello
+      </div>
     </div>
   );
 }
